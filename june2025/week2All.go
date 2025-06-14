9fb46be8a0971c4bd09400e8589498ee455bc7a1
devzeeh <jjohnroxas06@gmail.com>
2025-06-14 13:44:27 +0800

    Refine comments in week2All.go for clarity and consistency

--- june2025/week2All.go
+++ june2025/week2All.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	// Importing the week2 package to access its functions "moduleName/folderPath/packageName"
 	"GoPractice/june2025/week2"
 )
 
@@ -12,7 +13,7 @@ func init() {
 }
 
 func main() {
-	// This is the main function for week 2 of June 2025
+	// This is the main function for week 2 of June 2025 "packageName.functionName()"
 	fmt.Println("Result for june week 2 day1: ", week2.Day1())
 	fmt.Println("Result for june week 2 day2: ", week2.Day2())
 	fmt.Println("Result for june week 2 day2: ", week2.Day3toDay5())
