c48d84b206599a6e70096fcb7d969a9227906b41
devzeeh <jjohnroxas06@gmail.com>
2025-06-21 13:29:45 +0800

    Rename juneWeekly.go for June 2025 Weekly Mini Project

---
+++ weeklyProject/juneWeekly.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	// Importing the week2 package to access its functions "moduleName/folderPath/packageName"
+	"GoPractice/weeklyProject/june"
+)
+
+// automatically executed when a package is initialized
+func init() {
+	// This is the init function for week 2 of June 2025
+	fmt.Println("Welcome to June 2025 Weekly Mini Project!")
+}
+
+func main() {
+	// This is the main function "packageName.functionName()"
+	fmt.Println("Result for june week 1: ", june.Week1Result())
+	fmt.Println("Result for june week 2: ", june.Week2Result())
+	fmt.Println("Result for june week 3: ", june.Week3Result())
+}
