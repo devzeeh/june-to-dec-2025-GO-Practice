f0e1850c510623f3e7c995c326e16dff3a0cbee5
devzeeh <jjohnroxas06@gmail.com>
2025-06-21 13:14:35 +0800

    Add Fibonacci function and update weekJuneAll.go to include Fibonacci results

--- june2025/weekJuneAll.go
+++ june2025/weekJuneAll.go
@@ -20,16 +20,17 @@ func main() {
 	fmt.Println("For week to of July All About Go Installations")
 
 	time.Sleep(3000 * time.Millisecond) // Sleep for 3 seconds to simulate a delay
-	fmt.Println("For week 2 of july")
+	fmt.Println("\nFor week 2 of july")
 	time.Sleep(2000) 
 	fmt.Println("Result for june week 2 day1: ", week2.Day1())
 	fmt.Println("Result for june week 2 day2: ", week2.Day2())
 	fmt.Println("Result for june week 2 day3: ", week2.Day3toDay5())
 	
 	time.Sleep(3000 * time.Millisecond) // Sleep for 3 seconds to simulate a delay
-	fmt.Println("For week 2 of july")
+	fmt.Println("\nFor week 3 of july")
 	time.Sleep(2000) 
-	fmt.Println("\nResult for june week 2 day1: ", week3.Week3Result())
+	fmt.Println("Result for june week 2 day1: ", week3.Week3Result())
 	fmt.Println("Result for Switch Statement: ", week3.Switch())
 	fmt.Println("Result for For Loops: ", week3.ForLoops())
+	fmt.Println("Result for Fibonacci: ", week3.Fibonacci())
 }
