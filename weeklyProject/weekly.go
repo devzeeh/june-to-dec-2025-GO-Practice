62372418982c73a16232a482d467566cb75ec04f
devzeeh <jjohnroxas06@gmail.com>
2025-06-21 13:01:47 +0800

    Add output for June week 3 results in weekly.go

--- weeklyProject/weekly.go
+++ weeklyProject/weekly.go
@@ -16,4 +16,5 @@ func main() {
 	// This is the main function "packageName.functionName()"
 	fmt.Println("Result for june week 1: ", june.Week1Result())
 	fmt.Println("Result for june week 2: ", june.Week2Result())
+	fmt.Println("Result for june week 3: ", june.Week3Result())
 }
