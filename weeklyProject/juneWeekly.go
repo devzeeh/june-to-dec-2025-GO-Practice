cb97474b723febea0cb3423862662442d480ff98
devzeeh <jjohnroxas06@gmail.com>
2025-07-07 12:43:24 +0800

    Add Week1Result1 function to manage a movie list with add and display options

--- weeklyProject/juneWeekly.go
+++ weeklyProject/juneWeekly.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	// Importing the week2 package to access its functions "moduleName/folderPath/packageName"
+	"GoPractice/weeklyProject/july"
 	"GoPractice/weeklyProject/june"
 )
 
 // automatically executed when a package is initialized
 func init() {
 	// This is the init function for week 2 of June 2025
-	fmt.Println("Welcome to June 2025 Weekly Mini Project!")
+	fmt.Println("Welcome to June - December 2025 Weekly Mini Project!")
 }
 
 func main() {
@@ -18,4 +19,8 @@ func main() {
 	fmt.Println("Result for june week 2: ", june.Week2Result())
 	fmt.Println("Result for june week 3: ", june.Week3Result())
 	fmt.Println("Result for june week 4: ", june.Week4Result())
+
+	// For the month of July
+	fmt.Println("\n Weekly project for month of July")
+	july.Week1Result1()
 }
